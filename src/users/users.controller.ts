30860f3b058470974260c7c65cc8b1b5106159f0
qkdflrgs <qkdflrgs@gmail.com>
2024-09-11 16:07:41 +0900

    feat(swagger): add @nestjs/swagger to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,26 +1,49 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserDto } from 'src/common/dto/user.dto';
 
+@ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOkResponse({
+    type: UserDto,
+    description: '성공',
+  })
+  @ApiNotFoundResponse({
+    description: '유저 정보를 찾을 수 없습니다',
+  })
+  @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   postUsers(@Body() data: JoinRequestDto) {
     this.usersService.postUsers(data.email, data.nickname, data.password);
   }
 
+  @ApiOkResponse({
+    type: UserDto,
+    description: '성공',
+  })
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   logIn(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
     req.logOut();
