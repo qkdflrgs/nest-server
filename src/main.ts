6a0d8ce8741ffd65babab770f53196dab3d9767e
qkdflrgs <qkdflrgs@gmail.com>
2024-09-11 16:06:46 +0900

    feat(swagger): add SwaggerModule to App

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Slack APIs')
+    .setDescription('Slack 개발을 위한 API 문서입니다')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 
   console.log(`Listening on port ${port}`);
