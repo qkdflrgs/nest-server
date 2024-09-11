3569d086bce2090eed51cf8541e36728eeccfe41
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-09-11 16:09:10 +0900

    [Server] feat(swagger): add @nestjs/swagger to App (#7)
    * feat(swagger): install @nestjs/swagger
    
    * feat(swagger): add SwaggerModule to App
    
    * feat(swagger): add User Dto to common
    
    * feat(swagger): add @nestjs/swagger to DmsController
    
    * feat(swagger): add @nestjs/swagger to UsersController
    
    * feat(swagger): add @nestjs/swagger to WorkspacesController
    
    * feat(swagger): add ApiProperty to JoinRequestDto

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class JoinRequestDto {
+  @ApiProperty({
+    example: 'example@slack.com',
+    description: '이메일',
+    required: true,
+  })
   public email: string;
 
+  @ApiProperty({
+    example: 'litae',
+    description: '닉네임',
+    required: true,
+  })
   public nickname: string;
 
+  @ApiProperty({
+    example: 'exlitae123',
+    description: '비밀번호',
+    required: true,
+  })
   public password: string;
 }
