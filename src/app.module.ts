c1672e9a71bf8f458673afad441e232ece7bed73
qkdflrgs <qkdflrgs@gmail.com>
2024-09-11 15:20:59 +0900

    feat(channels): add Channels Module to App

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { DmsModule } from './dms/dms.module';
+import { ChannelsModule } from './channels/channels.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, DmsModule],
+  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, DmsModule, ChannelsModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
