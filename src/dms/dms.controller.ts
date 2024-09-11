4b9aaca9c8b334b37fd1cef60e82e20953aa4df3
qkdflrgs <qkdflrgs@gmail.com>
2024-09-11 16:07:30 +0900

    feat(swagger): add @nestjs/swagger to DmsController

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -1,8 +1,30 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ChatRequestDto } from './dto/chat.request.dto';
+import { ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('DM')
 @Controller('api/workspaces/:url/dms')
 export class DmsController {
+  @ApiParam({
+    name: 'url',
+    description: '워크스페이스 url',
+    required: true,
+  })
+  @ApiParam({
+    name: 'id',
+    description: '사용자 id',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'perPage',
+    description: '한 번에 가져오는 개수',
+    required: true,
+  })
+  @ApiQuery({
+    name: 'page',
+    description: '불러올 페이지',
+    required: true,
+  })
   @Get(':id/chats')
   getChat(@Query() query, @Param('id') id, @Param('url') url) {
     console.log(query.perPage, query.page, id, url);
