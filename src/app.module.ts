6d12589a3d5f4f21448f6b15ad29344881497b60
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-09-11 18:47:04 +0900

    [Server] feat(typeorm): set TypeOrm Module with entities (#10)
    * feat(typeorm): install typeorm-model-generator
    
    * feat(typeorm): install @nestjs/typeorm, mysql2
    
    * feat(typeorm): add entities
    
    * feat(typeorm): add TypeOrmModule to App

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,31 @@ import { UsersModule } from './users/users.module';
 import { WorkspacesModule } from './workspaces/workspaces.module';
 import { DmsModule } from './dms/dms.module';
 import { ChannelsModule } from './channels/channels.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './entities/Users';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, WorkspacesModule, DmsModule, ChannelsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    WorkspacesModule,
+    DmsModule,
+    ChannelsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: false,
+      logging: true,
+      keepConnectionAlive: true,
+      charset: 'utf8mb4',
+    }),
+    TypeOrmModule.forFeature([Users]),
+  ],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
