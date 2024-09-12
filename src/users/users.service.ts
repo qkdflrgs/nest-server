3a5fc28aebcd890a03422d510827d91a3a6b42cf
qkdflrgs <qkdflrgs@gmail.com>
2024-09-12 23:11:26 +0900

    feat(join): add join function to UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  postUsers(email: string, nickname: string, password: string) {}
+  constructor(
+    @InjectRepository(Users) private userRepository: Repository<Users>,
+  ) {}
+
+  getUser() {}
+
+  async join(email: string, nickname: string, password: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (user) {
+      // 이미 존재하는 유저 에러
+      throw new UnauthorizedException('이미 존재하는 유저입니다.');
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    await this.userRepository.save({
+      email,
+      nickname,
+      password: hashedPassword,
+    });
+  }
 }
