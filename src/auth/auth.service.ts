17340614ace7d6bf6f280a46f521b617bcbfb930
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-09-13 02:13:07 +0900

    [Server] feat(login): add Login API (#13)
    * feat(login): install @nestjs/passport
    
    * feat(login): install passport, passport-local
    
    * feat(login): add LoggedInGuard to auth
    
    * feat(login): add NotLoggedInGuard to auth
    
    * feat(login): add LocalAuthGuard to auth
    
    * feat(login): add LocalSerializer to auth
    
    * feat(login): add LocalStrategy to auth
    
    * feat(login): add AuthService
    
    * feat(login): add AuthModule
    
    * feat(login): add Guards to Users controller
    
    * feat(login): add passport to App
    
    * feat(login): add AuthModule to AppModule

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from 'src/entities/Users';
+import { Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(Users) private userRepository: Repository<Users>,
+  ) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'email', 'password', 'nickname'],
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+
+    if (result) {
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    }
+
+    return null;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 import {
+  ApiInternalServerErrorResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
@@ -9,6 +18,9 @@ import {
 } from '@nestjs/swagger';
 import { UserDto } from 'src/common/dto/user.dto';
 import { User } from 'src/common/decorators/user.decorator';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { LoggedInGuard } from 'src/auth/logged-in.guard';
+import { NotLoggedInGuard } from 'src/auth/not-logged-in.guard';
 
 @ApiTags('USER')
 @Controller('api/users')
@@ -22,12 +34,16 @@ export class UsersController {
   @ApiNotFoundResponse({
     description: '유저 정보를 찾을 수 없습니다',
   })
+  @ApiInternalServerErrorResponse({
+    description: '서버 에러',
+  })
   @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@User() user) {
-    return user;
+    return user || false;
   }
 
+  @UseGuards(new NotLoggedInGuard())
   @ApiOperation({ summary: '회원가입' })
   @Post()
   async join(@Body() body: JoinRequestDto) {
@@ -39,11 +55,13 @@ export class UsersController {
     description: '성공',
   })
   @ApiOperation({ summary: '로그인' })
+  @UseGuards(new LocalAuthGuard())
   @Post('login')
   logIn(@User() user) {
     return user;
   }
 
+  @UseGuards(new LoggedInGuard())
   @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logOut(@Req() req, @Res() res) {
