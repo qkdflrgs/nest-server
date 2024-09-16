cf42b477c03eaed415f2939e2df7c4e2c4483a7f
qkdflrgs <qkdflrgs@gmail.com>
2024-09-17 01:20:46 +0900

    api(channels): import modules to Channels Module

--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ChannelsService } from './channels.service';
 import { ChannelsController } from './channels.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Channels } from 'src/entities/Channels';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { ChannelChats } from 'src/entities/ChannelChats';
+import { Users } from 'src/entities/Users';
+import { Workspaces } from 'src/entities/Workspaces';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Channels,
+      ChannelMembers,
+      ChannelChats,
+      Users,
+      Workspaces,
+    ]),
+  ],
   providers: [ChannelsService],
   controllers: [ChannelsController],
 })
