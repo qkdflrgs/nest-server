047a424c674e9f3e26c25832f2f8d4a3665298d4
qkdflrgs <qkdflrgs@gmail.com>
2024-09-17 01:20:09 +0900

    api(channels): add controllers of Channels API

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,10 +1,18 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { ChatRequestDto } from 'src/dms/dto/chat.request.dto';
+import { ChannelsService } from './channels.service';
+import { User } from 'src/common/decorators/user.decorator';
 
+@ApiTags('CHANNEL')
 @Controller('api/workspaces/:url/channels')
 export class ChannelsController {
+  constructor(private channelsService: ChannelsService) {}
+
   @Get()
-  getAllChannels() {}
+  getAllChannels(@Param('url') url: string, @User() user) {
+    return this.channelsService.getWorkspaceChannels(url, user.id);
+  }
 
   @Post()
   createChannel() {}
@@ -16,6 +24,13 @@ export class ChannelsController {
   getChats(@Query('perPage') perPage, @Query('page') page, @Param() param) {
     console.log(perPage, page);
     console.log(param.id, param.url);
+
+    return this.channelsService.getWorkspaceChannelChats(
+      param.url,
+      param.name,
+      perPage,
+      page,
+    );
   }
 
   @Post(':name/chats')
@@ -23,11 +38,27 @@ export class ChannelsController {
     console.log(body.content);
   }
 
+  @Post(':name/images')
+  postImages(@Body() body: ChatRequestDto) {
+    console.log(body.content);
+  }
+
   @Get(':name/unread')
-  getNumberOfUnreadChats() {}
+  getUnreadMessages(
+    @Param('url') url: string,
+    @Param('name') name: string,
+    @Query('after') after: number,
+  ) {
+    return this.channelsService.getChannelUnreadCount(url, name, after);
+  }
 
   @Get(':name/members')
-  getAllMembersOfChannel() {}
+  getAllMembersOfChannel(
+    @Param('url') url: string,
+    @Param('name') name: string,
+  ) {
+    return this.channelsService.getWorkspaceChannelMembers(url, name);
+  }
 
   @Post(':name/members')
   inviteMemberToChannel() {}
