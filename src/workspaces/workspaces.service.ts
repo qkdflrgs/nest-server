6071a494ff9c65b05d866856faa1de444dc9602a
qkdflrgs <qkdflrgs@gmail.com>
2024-09-16 15:10:09 +0900

    feat(workspace): add Workspace APIs

--- src/workspaces/workspaces.service.ts
+++ src/workspaces/workspaces.service.ts
@@ -1,4 +1,135 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ChannelMembers } from 'src/entities/ChannelMembers';
+import { Channels } from 'src/entities/Channels';
+import { Users } from 'src/entities/Users';
+import { WorkspaceMembers } from 'src/entities/WorkspaceMembers';
+import { Workspaces } from 'src/entities/Workspaces';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
-export class WorkspacesService {}
+export class WorkspacesService {
+  constructor(
+    @InjectRepository(Workspaces)
+    private workspacesRepository: Repository<Workspaces>,
+    @InjectRepository(Channels)
+    private channelsRepository: Repository<Channels>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+    private dataSource: DataSource,
+  ) {}
+
+  async findById(id: number) {
+    return this.workspacesRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async findMyWorkspaces(myId: number) {
+    return this.workspacesRepository.find({
+      where: {
+        WorkspaceMembers: [{ UserId: myId }],
+      },
+    });
+  }
+
+  async createWorkspace(name: string, url: string, myId: number) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    const workspace = this.workspacesRepository.create({
+      name,
+      url,
+      OwnerId: myId,
+    });
+
+    try {
+      const returned = await queryRunner.manager
+        .getRepository(Workspaces)
+        .save(workspace);
+
+      const workspaceMember = new WorkspaceMembers();
+      workspaceMember.UserId = myId;
+      workspaceMember.WorkspaceId = returned.id;
+
+      const channel = new Channels();
+      channel.name = '일반';
+      channel.WorkspaceId = returned.id;
+
+      const [, channelReturned] = await Promise.all([
+        queryRunner.manager
+          .getRepository(WorkspaceMembers)
+          .save(workspaceMember),
+        queryRunner.manager.getRepository(Channels).save(channel),
+      ]);
+
+      const channelMember = new ChannelMembers();
+      channelMember.UserId = myId;
+      channelMember.ChannelId = channelReturned.id;
+
+      await queryRunner.manager
+        .getRepository(ChannelMembers)
+        .save(channelMember);
+    } catch (error) {
+      console.error(error);
+      await queryRunner.rollbackTransaction();
+    } finally {
+      await queryRunner.release();
+    }
+  }
+
+  async getWorkspaceMembers(url: string) {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .innerJoin('user.WorkspaceMembers', 'member')
+      .innerJoin('member.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getMany();
+  }
+
+  async createWorkspaceMembers(url: string, email: string) {
+    const workspace = await this.workspacesRepository
+      .createQueryBuilder('workspace')
+      .where('workspace.url = :url', { url })
+      .innerJoinAndSelect('workspace.Channels', 'channels')
+      .getOne();
+
+    const user = await this.usersRepository.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    const workspaceMember = new WorkspaceMembers();
+    workspaceMember.WorkspaceId = workspace.id;
+    workspaceMember.UserId = user.id;
+
+    await this.workspaceMembersRepository.save(workspaceMember);
+
+    const channelMember = new ChannelMembers();
+    channelMember.ChannelId = workspace.Channels.find(
+      (channel) => channel.name === '일반',
+    ).id;
+    channelMember.UserId = user.id;
+
+    await this.channelMembersRepository.save(channelMember);
+  }
+
+  async getWorkspaceMember(url: string, id: number) {
+    return this.usersRepository
+      .createQueryBuilder('user')
+      .where({ id })
+      .innerJoin('user.Workspace', 'workspace', 'workspace.url = :url', {
+        url,
+      })
+      .getOne();
+  }
+}
