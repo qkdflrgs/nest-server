53ec432d7e5bac908b70152b796314f2c99b8aab
qkdflrgs <qkdflrgs@gmail.com>
2024-09-20 16:10:49 +0900

    feat(websocket): change file naming of Http exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { HttpExceptionFilter } from './httpException.filter';
+import { HttpExceptionFilter } from './http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import passport from 'passport';
 
