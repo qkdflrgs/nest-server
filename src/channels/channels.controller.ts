7a3d4763eecea41d8fe9a4de1ad0056a0ec4c554
qkdflrgs <qkdflrgs@gmail.com>
2024-09-21 17:47:43 +0900

    feat(uploadImages): add FilesInterceptor to ChannelsController

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,8 +1,28 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ChannelsService } from './channels.service';
 import { User } from 'src/common/decorators/user.decorator';
 import { PostChatDto } from './dto/post-chat.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import fs from 'fs';
+import multer from 'multer';
+import path from 'path';
+
+try {
+  fs.readdirSync('uploads');
+} catch {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
 
 @ApiTags('CHANNEL')
 @Controller('api/workspaces/:url/channels')
@@ -48,9 +68,33 @@ export class ChannelsController {
     });
   }
 
+  @UseInterceptors(
+    FilesInterceptor('image', 10, {
+      storage: multer.diskStorage({
+        destination(req, file, cb) {
+          cb(null, 'uploads/');
+        },
+        filename(req, file, cb) {
+          const ext = path.extname(file.originalname);
+          cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+        },
+      }),
+      limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    }),
+  )
   @Post(':name/images')
-  postImages(@Body() body) {
-    console.log(body.content);
+  postImages(
+    @UploadedFiles() files: Express.Multer.File[],
+    @Param('url') url: string,
+    @Param('name') name: string,
+    @User() user,
+  ) {
+    return this.channelsService.createWorkspaceChannelImages({
+      url,
+      name,
+      files,
+      myId: user.id,
+    });
   }
 
   @Get(':name/unread')
